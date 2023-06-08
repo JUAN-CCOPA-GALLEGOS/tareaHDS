761cc605575df06ca28e92fd2f5866ae436c8fb4
JUAN GALLEGOS <135194695+JUAN-CCOPA-GALLEGOS@users.noreply.github.com>
2023-06-08 14:16:20 -0500

    correccion 3_3.cpp

--- 3_3.cpp
+++ 3_3.cpp
@@ -1,28 +1,35 @@
 #include <iostream>
+#include <cmath>
+
 using namespace std;
-int main(){
-float a, b, c, d, e, f, denominador, x, y;
-cout << "ingresa el valor de a: ";
-cin>> a;
-cout <<"ingresa el valor de b: ";
-cin>> b;
-cout<< "ingresa el valor de c: ";
-cin>> c;
-cout << "ingresa el valor de d: ";
-cin>> d;
-cout <<"ingresa el valor de e: ";
-cin>> e;
-cout<<"ingresa el valor de f: ";
-cin>> f;
-denominador = a * e - b * d;
-if (denominador == 0)
-cout << " no hay solucion";
-else
-{
-x = (c * e - b * f) / denominador;
-y = (a * f - c * d) / denominador;
-cout << " la solucion del sistema es";
-cout << " x = " << x << " y = " << y << endl;
-}
-return 0;
+
+int main() {
+    float a, b, c, d, e, f, denominador, x, y;
+    cout << "Ingresa el valor de a: ";
+    cin >> a;
+    cout << "Ingresa el valor de b: ";
+    cin >> b;
+    cout << "Ingresa el valor de c: ";
+    cin >> c;
+    cout << "Ingresa el valor de d: ";
+    cin >> d;
+    cout << "Ingresa el valor de e: ";
+    cin >> e;
+    cout << "Ingresa el valor de f: ";
+    cin >> f;
+    denominador = a * e - b * d;
+    if (denominador == 0) {
+        cout << "No hay solucion" << endl;
+    } else {
+        x = (c * e - b * f) / denominador;
+        if (x > 2) {
+            y = pow((a * f - c * d) / denominador, 2);
+        } else {
+            y = (a * f - c * d) / denominador - 2;
+        }
+        cout << "La solucion del sistema es: " << endl;
+        cout << "x = " << x << endl;
+        cout << "y = " << y << endl;
+    }
+    return 0;
 } // MODIFICAR EL EJERCICIO PARA EL CASO QUE SI EL RESULTADO DE "X" ES MAYOR QUE 2, EL RESULTADO DE "Y", DEBERA SER ELEVADO AL CUADRADO, CASO CONTRARIO SE LE RESTARA 2 AL ULTIMO VALOR DE "Y"
